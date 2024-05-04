e3cec1f998817507ee141a1805dc39d7c8114f54
Mohammad Ali <61760042+mohdalipradhan@users.noreply.github.com>
2024-05-04 08:26:20 +0530

    Update NavBar.tsx

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,19 +1,22 @@
 import { Flex, Stack, Text } from '@chakra-ui/react'
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <Flex p={1}  width="100%">
-        <Flex  justify="flex-start" align="center" flex={2}>
-            <Text marginLeft="38px" fontSize="15px" fontWeight={700}>Hi, AltWorld</Text>
+    <Flex p={1} width="100%">
+      <Flex marginLeft="38px" gap={2}  justify="flex-start" align="center" flex={2}>
+        <FontAwesomeIcon width="16px"  icon={faKeyboard} />
+        <Text  fontSize="15px" fontWeight={700}> Hi, AltWorld</Text>
+      </Flex>
+      <Stack flex={8}>
+        <Flex>
+          <Text fontSize="13px" color="gray" fontWeight={500}>Pages/</Text>
+          <Text fontSize="13px" fontWeight={500}>Assignment</Text>
         </Flex>
-        <Stack flex={8}>
-            <Flex>
-                <Text fontSize="13px" color="gray" fontWeight={500}>Pages/</Text>
-                <Text fontSize="13px"  fontWeight={500}>Assignment</Text>
-            </Flex>
-            <Text  fontWeight={700} fontSize="13px">Sales BDE</Text>
-        </Stack>
+        <Text fontWeight={700} fontSize="13px">Sales BDE</Text>
+      </Stack>
     </Flex>
   )
 }
